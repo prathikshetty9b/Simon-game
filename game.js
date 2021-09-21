6b4af55baec211e95002583bdab43aea08ddc5f6
Prathik B Shetty <prathikshetty9b@gmail.com>
2021-09-21 11:48:56 +0530

    add comment

--- game.js
+++ game.js
@@ -1,11 +1,19 @@
+// Array to store button colors
 var buttonColors = new Array("red","blue","yellow","green");
+// Array to store button sequence
 var gamePattern = [];
 
 function nextSequence(){
+    //generate radom number from 0 - 3
     var randomNumber = Math.floor(Math.random()*4);
+    
+    //choose one random color from buttonColors
     var randomChosenColor = buttonColors[randomNumber];
+    // store the sequence in an array
     gamePattern.push(randomChosenColor);
     $("#" + randomChosenColor).fadeOut(50).fadeIn(50).fadeOut(50).fadeIn(50);
+
+    // Mapp sound  to corresponding button.
     var playSound = new Audio("./sounds/" + randomChosenColor + ".mp3");
     playSound.play();
     
