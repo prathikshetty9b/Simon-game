346138d9b4208370bb8c6bcee1bb2c7abc9054d3
Prathik B Shetty <prathikshetty9b@gmail.com>
2021-09-21 12:44:07 +0530

    add button animation & sound

--- game.js
+++ game.js
@@ -2,6 +2,22 @@
 var buttonColors = new Array("red","blue","yellow","green");
 // Array to store button sequence
 var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+    var userChosenColor = $(this).attr("id");
+    /*
+    $(this).addClass("pressed");
+    setTimeout(function(){
+        $(this).removeClass("pressed");
+    },100);*/
+    userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);
+    console.log(userClickedPattern);
+
+    //play sound on button click
+    playSound(userChosenColor);
+})
 
 function nextSequence(){
     //generate radom number from 0 - 3
@@ -11,11 +27,24 @@ function nextSequence(){
     var randomChosenColor = buttonColors[randomNumber];
     // store the sequence in an array
     gamePattern.push(randomChosenColor);
+
+
     $("#" + randomChosenColor).fadeOut(50).fadeIn(50).fadeOut(50).fadeIn(50);
 
     // Mapp sound  to corresponding button.
-    var playSound = new Audio("./sounds/" + randomChosenColor + ".mp3");
+    playSound(randomChosenColor);
+    
+}
+
+function playSound(name){
+    var playSound = new Audio("./sounds/" + name + ".mp3");
     playSound.play();
     
 }
 
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed"); 
+    },100);
+}
